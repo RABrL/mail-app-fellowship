efeee96e03165eb1d4a110b0b481cb162e458e34
scarolayton <scarolayton@gmail.com>
2024-02-22 09:23:53 -0500

    starting with the emails view

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,11 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       colors: {
         principal: '#303052',
         button: '#fe6c91',
