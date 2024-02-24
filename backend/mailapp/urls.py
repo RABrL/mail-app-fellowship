295fece191da7d47188bc24decc1fed138fa64c5
JuanEstebanR <juan1010.jerm@gmail.com>
2024-02-24 14:26:54 -0500

    Fixed urls file

--- backend/mailapp/urls.py
+++ backend/mailapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views.views import *
 
 urlpatterns = [
-    path('mails/get/received/<str:user_mail>/', MailsReceivedUserGetterEndpoint.as_view()),
-    path('mails/get/sent/<str:user_mail>/', MailsSentUserGetterEndpoint.as_view()),
-    path('mail/get/information/<str:mail_id>', InformationForMailGetterEndpoint.as_view()),
-    path('mail/post/mail/', SendMailPostEndpoint.as_view()),
+    path('get/received/<str:user_mail>/', MailsReceivedUserGetterEndpoint.as_view()),
+    path('get/sent/<str:user_mail>/', MailsSentUserGetterEndpoint.as_view()),
+    path('get/information/<str:mail_id>', InformationForMailGetterEndpoint.as_view()),
+    path('post/mail/', SendMailPostEndpoint.as_view()),
     path('user/post/create/', CreateUserPostEndpoint.as_view()),
     path('user/get/authentication/<str:email>/<str:password>/', AuthenticationUserGetterEndpoint.as_view()),
 ]
