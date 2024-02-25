bbe03d54075fcab9c7ee150cf18aa3e6789896d5
JuanEstebanR <juan1010.jerm@gmail.com>
2024-02-24 20:21:43 -0500

    update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 # Application definition
 
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'mailapp.apps.MailappConfig',  # Add this line to the INSTALLED_APPS list
+]
+
+CORS_ALLOWED_ORIGINS = [
+    '*',
 ]
 
 MIDDLEWARE = [
@@ -82,16 +86,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'email_db',
-        'HOST': 'localhost',
-        'PASSWORD': 'postgres',
-        'USER': 'postgres',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'email_db',
+#         'HOST': 'localhost',
+#         'PASSWORD': 'postgres',
+#         'USER': 'postgres',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
