49773cd249a950507af7e07ba44c46c5fac9281a
JuanEstebanR <juan1010.jerm@gmail.com>
2024-02-24 20:24:01 -0500

    update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,16 +86,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'email_db',
-#         'HOST': 'localhost',
-#         'PASSWORD': 'postgres',
-#         'USER': 'postgres',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'email_db',
+        'HOST': 'localhost',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': 'postgres',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
