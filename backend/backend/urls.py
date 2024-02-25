34e8aa22eebf458ab8920dd48565c7816a539d36
JuanEstebanR <juan1010.jerm@gmail.com>
2024-02-24 20:34:08 -0500

    Fix urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mailapp.views import *
+from mailapp.views.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
