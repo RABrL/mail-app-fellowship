bbcb80daed11d760c9c5f4d6cfadec41e1b6b53a
RABrL <robin_brito@hotmail.com>
2024-02-24 20:07:26 -0500

    Update ALLOWED_HOSTS in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -82,16 +82,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'email_db',
-        'HOST': 'localhost',
-        'PASSWORD': 'postgres',
-        'USER': 'postgres',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
