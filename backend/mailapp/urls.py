a67dbec326727719229d6990a6e3680a9c7be0e0
JuanEstebanR <juan1010.jerm@gmail.com>
2024-02-27 17:28:51 -0500

    Update the mailApp urls

--- backend/mailapp/urls.py
+++ backend/mailapp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views.views import *
 
 urlpatterns = [
-    path('received/<str:user_mail>', MailsReceivedUserGetterEndpoint.as_view()),
-    path('sent/<str:user_mail>', MailsSentUserGetterEndpoint.as_view()),
-    path('information/<str:mail_id>', InformationForMailGetterEndpoint.as_view()),
-    path('', SendMailPostEndpoint.as_view()),
+    path('received/<str:user_mail>/', MailsReceivedUser.as_view(), name='mails_received'),
+    path('sent/<str:user_mail>', MailsSentUser.as_view(), name='mails_sent'),
+    path('information/<str:mail_id>', InformationForMail.as_view(), name='mail_information'),
+    path('', SendMail.as_view(), name='send_mail'),
+    path('user/', CreateUser.as_view(), name='create_user'),
+    path('user/authentication/<str:email>/<str:password>', AuthenticationUserGetterEndpoint.as_view(), name='authentication_user'),
 ]
