c5c0eaee10b4ad469dd15799db873e1a5c6200e5
JuanEstebanR <juan1010.jerm@gmail.com>
2024-02-27 19:14:04 -0500

    Create folders urls

--- backend/mailapp/urls.py
+++ backend/mailapp/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', SendMail.as_view(), name='send_mail'),
     path('user/', CreateUser.as_view(), name='create_user'),
     path('user/authentication/<str:email>/<str:password>', AuthenticationUserGetterEndpoint.as_view(), name='authentication_user'),
+    path('create_folder/', CreateFolder.as_view(), name='create_folder'),
+    path('save_mails/', SaveEmailsInFolder.as_view(), name='save_mails_in_folder'),
+    path('folder/<str:folder_name>/<str:user_email>', GetEmailsFolder.as_view(), name='get_emails_folder'),
 ]
