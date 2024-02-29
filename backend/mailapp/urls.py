0fc3083dc02a66ccb9c0429cca735a51fd7e54ee
JuanEstebanR <juan1010.jerm@gmail.com>
2024-02-29 00:29:48 -0500

    Update views names in urlpatterns

--- backend/mailapp/urls.py
+++ backend/mailapp/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views.views import *
 
 urlpatterns = [
-    path('received/<str:user_mail>/', MailsReceivedUser.as_view(), name='mails_received'),
-    path('sent/<str:user_mail>', MailsSentUser.as_view(), name='mails_sent'),
-    path('information/<str:mail_id>', InformationForMail.as_view(), name='mail_information'),
-    path('', SendMail.as_view(), name='send_mail'),
-    path('user/', CreateUser.as_view(), name='create_user'),
-    path('user/authentication/<str:email>/<str:password>', AuthenticationUserGetterEndpoint.as_view(), name='authentication_user'),
-    path('create_folder/', CreateFolder.as_view(), name='create_folder'),
-    path('save_mails/', SaveEmailsInFolder.as_view(), name='save_mails_in_folder'),
-    path('folder/<str:folder_name>/<str:user_email>', GetEmailsFolder.as_view(), name='get_emails_folder'),
+    path('received/<str:user_mail>/', MailsListView.as_view(), name='mails_list'),
+    path('sent/<str:user_mail>', MailsSentView.as_view(), name='mails_sent'),
+    path('information/<str:mail_id>', MailInfoView.as_view(), name='mail_info'),
+    path('', SendMailView.as_view(), name='send_mail'),
+    path('user/', CreateUserView.as_view(), name='create_user'),
+    path('user/authentication/', AuthenticationUserView.as_view(), name='authentication_user'),
+    path('create_folder/', CreateFolderView.as_view(), name='create_folder'),
+    path('save_mails/', SaveEmailsView.as_view(), name='save_mails_in_folder'),
+    path('folder/<str:folder_name>/<str:user_email>', EmailsFolderView.as_view(), name='folder_mails'),
 ]
