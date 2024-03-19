196b322ca341795eb8e940962719eb30ef37d182
Carlos Forgiony <carlos.forgiony@gmail.com>
2024-03-19 03:48:28 -0500

    *Refactored api calls to work with axios. *Added api-client making api calls easier to read. *Added emailCheck to remove clutter from the api calls.

--- frontend/services/getEmails.ts
+++ frontend/services/getEmails.ts
@@ -1,34 +1,29 @@
-import { Email } from '@/types'
-import axios from 'axios'
+import { Email } from "@/types";
+import apiClient from "./api-client";
 
 export const getEmailsByUser = async (userEmail: string) => {
   try {
-    return await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/mail/received/${userEmail}`
-    )
+    return await apiClient.get(`/mail/received/${userEmail}`);
   } catch (error) {
-    console.error()
+    console.error();
   }
-}
+};
 export const getEmailsSent = async (userEmail: string) => {
   try {
-    return await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/mail/sent/${userEmail}`
-    )
+    return await apiClient.get(`/mail/sent/${userEmail}`);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 export const getAnEmail = async (
   mailId: number
 ): Promise<Email | undefined> => {
   try {
-    const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/mail/information/${mailId}`
-    )
-    if (!res.data) return undefined
-    return res.data
+    const res = await apiClient.get(`/mail/information/${mailId}`);
+    if (!res.data) return undefined;
+    const data = await res.data;
+    return data;
   } catch (error) {
-    console.error()
+    console.error();
   }
-}
+};
--- frontend/services/sendEmail.ts
+++ frontend/services/sendEmail.ts
@@ -1,38 +1,33 @@
+import { validReceiver } from "@/utils/emailCheck";
+import apiClient from "./api-client";
+
 export async function sendEmail(formData: FormData, sender_email: string) {
-  const receiver = formData.get('receiver')?.toString()
-  const content = formData.get('content')?.toString()
-  const subject = formData.get('subject')?.toString()
+  const receiver = formData.get("receiver")?.toString();
+  const content = formData.get("content")?.toString();
+  const subject = formData.get("subject")?.toString();
 
   if (!receiver || !content || !subject)
-    return [new Error('Todos los campos son requeridos'), null]
+    return [new Error("Todos los campos son requeridos"), null];
 
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  if (!validReceiver(receiver))
+    return [new Error("El correo no es valido"), null];
 
-  if (!emailRegex.test(receiver))
-    return [new Error('El correo no es valido'), null]
-
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/mail/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      subject,
-      content,
-      sender_email,
-      receiver_email: receiver
-    })
-  })
+  const res = await apiClient.post(`/mail/`, {
+    subject,
+    content,
+    sender_email,
+    receiver_email: receiver,
+  });
 
   if (res.status === 404) {
-    return [new Error(`The recipient's e-mail does not exist`), null]
+    return [new Error(`The recipient's e-mail does not exist`), null];
   }
 
-  if (!res.ok) {
-    return [new Error('Error sendig mail'), null]
+  if (res.status !== 201) {
+    return [new Error("Error sendig mail"), null];
   }
 
-  const data = await res.json()
+  const data = await res.data;
 
-  return [null, data]
+  return [null, data];
 }
