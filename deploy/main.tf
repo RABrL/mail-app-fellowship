10e14a02b54578b80e5272ffd8d371f57021c2ab
imitelis <dsaavedra@unal.edu.co>
2024-03-23 21:36:16 -0500

    Larger EC2 instance and fixed frontend pipeline

--- deploy/main.tf
+++ deploy/main.tf
@@ -34,24 +34,6 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-/*
-# RDS PostgreSQL instance
-resource "aws_db_instance" "postgresql" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "14"
-  instance_class       = "db.t3.micro"
-  username             = var.db_username
-  password             = var.db_password
-  db_name              = "mail_db"
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-}
-*/
-
 # Aurora PostgreSQL cluster
 resource "aws_rds_cluster" "aurora_postgresql" {
   cluster_identifier           = "app-aurora-cluster"
@@ -91,27 +73,36 @@ resource "aws_rds_cluster_instance" "aurora_instance_replica" {
 # EC2 Instance
 resource "aws_instance" "ec2" {
   ami             = "ami-080e1f13689e07408"
-  instance_type   = "t2.micro"
-  key_name        = var.ec2_key_name
+  instance_type   = "t2.small"
+  key_name        = "terraform-key"
   security_groups = [aws_security_group.app_sg.name]
-
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install -y docker.io git
               sudo systemctl start docker
               sudo systemctl enable docker
-
               sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
               sudo chmod +x /usr/local/bin/docker-compose
-
-              git clone https://github.com/RABrL/mail-app-fellowship
+              git clone https://github.com/rABrL/mail-app-fellowship
               cd mail-app-fellowship
-
-              # sudo docker-compose up
-              
+              export DEV_ALLOWED_HOST=0.0.0.0:8000
+              export PROD_ALLOWED_HOST=0.0.0.0:8000
+              export DEV_ALLOWED_ORIGIN=http://localhost:3000
+              export PROD_ALLOWED_ORIGIN=http://localhost:3000
+              export NEXT_PUBLIC_API_URL=http://localhost:8000          
+              export DJANGO_SECRET_KEY=
+              export DB_PASSWORD=
+              export DB_HOST=
+              export SMTP_USER=
+              export SMTP_PASSWORD=
+              export SMTP_SERVER=
+              export SMTP_PORT=
+              sudo docker-compose up --build
               EOF
-
+  root_block_device{
+    volume_size=12
+  }
   tags = {
     Name = "mail-ec2"
   }
